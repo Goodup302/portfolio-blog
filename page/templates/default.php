4b68b4f2453a1b351ca33b771ea8af1d25fa0757
Goodup302 <j.f0471430704@gmail.com>
2018-11-29 16:30:23 +0100

    Reset default template
    Signed-off-by: Goodup302 <j.f0471430704@gmail.com>

--- page/templates/default.php
+++ page/templates/default.php
@@ -8,10 +8,11 @@ $dbuser = new \Core\Auth\DBAuth(App::getInstance()->getDatabase());
 <head>
     <title><?= $app->getTitle() ?></title>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
+
     <nav class="col-md-12 d-flex">
         <h1><?= $app->getTitle() ?></h1>
         <?php
