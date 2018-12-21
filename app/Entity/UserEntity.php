020920c89891540ab8a81d45e4d4c0ae255554b1
Goodup302 <j.f0471430704@gmail.com>
2018-12-21 16:36:30 +0100

    Mail Class has change
    Signed-off-by: Goodup302 <j.f0471430704@gmail.com>

--- app/Entity/UserEntity.php
+++ app/Entity/UserEntity.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use Core\Config;
 use Core\Entity\Entity;
+use Core\Form\Email;
 
 class UserEntity extends Entity
 {
 
-    public function getAdmin()
+    public function getAdmin() : bool
     {
         return boolval($this->admin);
     }
@@ -20,5 +22,23 @@ class UserEntity extends Entity
             return true;
         }
         return false;
+    }
+
+    public function sendConfirmMail()
+    {
+        $receiver = $this->email;
+        $sender = Config::getInstance(CONFIG_FILE)->get('validation_mail');
+        $objet = "Activation du compte : {$_SERVER['SERVER_NAME']}";
+        $name = $this->data['username'];
+        $content = $this->data['message'];
+        //
+        $message = '<p>Sujet : ' . $objet . '<br />';
+        $message .= 'Email : ' . $sender . '</p>';
+        $message .= '<p>' . htmlspecialchars($content) . '</p>';
+        $Mail = new Email("$name vous contact", $sender);
+        $result = $Mail->SendMail($receiver, 'Contact de ' . $name, $message);
+        if (!$result) {
+            $this->error_message = "Le mail n'a pas pu ètre envoyé, réessayer plus tard.";
+        }
     }
 }
--- core/Form/Email.php
+++ core/Form/Email.php
@@ -8,23 +8,48 @@ class Email
 
     private $header;
 
-    function __construct($email_title, $email_sender) {
-        $this->header="MIME-Version: 1.0\r\n";
-        $this->header.='From:"' . $email_title . '"<' . $email_sender . '>'."\n";
-        $this->header.='Content-Type:text/html; charset="utf-8"'."\n";
-        $this->header.='Content-Transfer-Encoding: 8bit';
+    //Mail
+    private $sender;
+    private $receiver;
+    //Args
+    private $subject;
+    private $htmlContent;
+
+    public function __construct($email_title, $email_sender)
+    {
+        $this->header = "MIME-Version: 1.0\r\n";
+        $this->header .= 'From:"' . $email_title . '"<' . $email_sender . '>' . "\n";
+        $this->header .= 'Content-Type:text/html; charset="utf-8"' . "\n";
+        $this->header .= 'Content-Transfer-Encoding: 8bit';
     }
 
-    public function SendMail($email, $titre, $message) {
-        $content='
-			<html>
-				<body>
-			';
-        $content.= $message;
-        $content.='
-				</body>
-			</html>
-			';
-        return mail($email, $titre, $content, $this->header);
+    public function setMail($sender, $receiver)
+    {
+        $this->sender = $sender;
+        $this->receiver = $receiver;
+        return $this;
+    }
+
+    public function setObjet($subject)
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    public function setContent($content)
+    {
+        $this->htmlContent = "
+		<html>
+			<body>
+                $content
+			</body>
+		</html>
+		";
+        return $this;
+    }
+
+    public function send()
+    {
+        return mail($this->receiver, $this->subject, $this->htmlContent, $this->header);
     }
 }
