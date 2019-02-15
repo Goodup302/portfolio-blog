9454b7ffaf6725dd1dc7e2f86d5a8c4cf8124d42
Goodup302 <j.f0471430704@gmail.com>
2019-02-15 10:33:14 +0100

    Add can change author of a post Fix avatar on comment add exerpt on post in loop page
    Signed-off-by: Goodup302 <j.f0471430704@gmail.com>

--- app/Entity/CommentEntity.php
+++ app/Entity/CommentEntity.php
@@ -40,13 +40,12 @@ class CommentEntity
     public function getLogo()
     {
         $logo = array(
-            'https://upload.wikimedia.org/wikipedia/commons/e/e7/Photo_de_profil_JK.jpg',
-            'http://media.lemeilleurjobdete.com/uploads/3656/profil-1435853876.jpg',
-            'https://images.vinted.net/thumbs/04bf8_hMYjmUes6GLatryL2Xc9xNRY.jpeg',
-            'https://pbs.twimg.com/profile_images/1015063956614938624/uxfcTNNt.jpg',
-            'https://vignette.wikia.nocookie.net/yandere-simulator-fr/images/0/06/Profil_Genka_Kunahito.png/revision/latest?cb=20171221192806&path-prefix=fr',
-            'http://s3.amazonaws.com/academos/production/mentor_profiles/photo/medium_AF_2018_-_profil_sociaux.jpg'
+            'images/avatar/1.jpg',
+            'images/avatar/2.jpg',
+            'images/avatar/3.jpg',
+            'images/avatar/4.jpg',
+            'images/avatar/5.jpg'
         );
-        return $logo[rand(0, sizeof($logo))];
+        return $logo[rand(0, sizeof($logo) - 1)];
     }
 }
